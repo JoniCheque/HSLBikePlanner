f0e0d856ac533a2cc9a55b8076d2f6b5f2be3b41
JoniCheque <joni@sekki.fi>
2023-03-04 14:46:34 +0200

    chore(backend): add initial version of backend

---
+++ HSLBackEnd/API/DTOs/Favourites.cs
@@ -0,0 +1,10 @@
+﻿namespace API.DTOs
+{
+    public class Favourites
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Latidute { get; set; }
+        public decimal Longidute { get; set; }
+    }
+}
