dc1e0ae87ff448047fe17c7abd29ece68070cbab
JoniCheque <joni@sekki.fi>
2023-03-04 15:30:20 +0200

    chore(bootstrap): add bootstrap and react-bootstrap

--- hslbikeplannerfrontend/src/app/app.tsx
+++ hslbikeplannerfrontend/src/app/app.tsx
@@ -1,5 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 import NxWelcome from './nx-welcome';
 
