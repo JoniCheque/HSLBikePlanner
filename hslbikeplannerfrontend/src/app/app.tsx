de4e76958f2e5f6112d4e86e792dae602ca4f820
JoniCheque <joni@sekki.fi>
2023-03-04 16:18:26 +0200

    feat(routeplanning): add inputs without logic

--- hslbikeplannerfrontend/src/app/app.tsx
+++ hslbikeplannerfrontend/src/app/app.tsx
@@ -1,15 +1,33 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
+import React from 'react';
+// import styles from './app.module.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 import NxWelcome from './nx-welcome';
+import Routeplanning from './routeplanning/routeplanning';
+import Map from './map/map';
+import Favourite from './favourite/favourite';
 
 export function App() {
   return (
     <>
-      <NxWelcome title="hslbikeplannerfrontend" />
-
+      <Container fluid>
+        <NxWelcome title="HSL Bike Route Planner" />
+        <Routeplanning />
+        <Row>
+          <Col>
+            <Favourite></Favourite>
+          </Col>
+          <Col>
+            <Map></Map>
+          </Col>
+        </Row>
+      </Container>
       <div />
     </>
   );
