c140b8d52eaa70528116e718a2daab0f97421276
JoniCheque <joni@sekki.fi>
2023-03-04 16:18:26 +0200

    feat(routeplanning): add inputs without logic

--- hslbikeplannerfrontend/src/app/routeplanning/routeplanning.tsx
+++ hslbikeplannerfrontend/src/app/routeplanning/routeplanning.tsx
@@ -1,12 +1,51 @@
+import React from 'react';
 import styles from './routeplanning.module.css';
+
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 /* eslint-disable-next-line */
 export interface RouteplanningProps {}
 
 export function Routeplanning(props: RouteplanningProps) {
+  function handleSubmit() {
+    console.log("Submit was clicked!");
+  }
+
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to Routeplanning!</h1>
+    <div>
+      <h5>Reittisuunnittelu</h5>
+      <form method='' onSubmit={handleSubmit}>
+        <Row>
+          <Col md>
+            <InputGroup size='lg' className='mb-3'>
+              <InputGroup.Text id='inputGroup-sizing-sm'>Lähtöpiste</InputGroup.Text>
+              <Form.Control
+                placeholder='Lähtöpiste'
+                aria-label='Small'
+                aria-describedby="inputGroup-sizing-default"
+                name='startingPoint'/>
+            </InputGroup>
+          </Col>
+          <Col md>
+            <InputGroup size='lg' className='mb-3'>
+            <InputGroup.Text id='inputGroup-sizing-sm'>Määränpää</InputGroup.Text>
+            <Form.Control
+              placeholder='Määränpää'
+              aria-label='Small'
+              aria-describedby="inputGroup-sizing-default"
+              name='startingPoint'/>
+              <Button variant="outline-secondary" id="button-addon2" type='submit'>
+                Hae reitti!
+              </Button>
+            </InputGroup>
+          </Col>
+        </Row>
+      </form>
     </div>
   );
 }
