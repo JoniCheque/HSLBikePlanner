737b2abcdaf8ce5f923597f68abf991742173ecd
JoniCheque <joni@sekki.fi>
2023-03-07 21:05:26 +0200

    chore(seeding): remove temp seeding implementation

--- HSLBackEnd/Repository/Program.cs
+++ HSLBackEnd/Repository/Program.cs
@@ -7,52 +7,6 @@ namespace Repository
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Start seeding!");
-            Console.WriteLine("Reading all the datasets:");
-
-
-            var directories =
-                Directory.GetDirectories("C:\\Users\\super\\koodit\\digitransit\\HSLBackEnd\\Repository\\Seeding\\DataFiles\\")
-                .ToList();
-
-            var allFiles = new List<string>();
-
-            foreach (string directory in directories)
-            {
-                var folderFiles = Directory.GetFiles(directory).ToList();
-                Console.WriteLine($"Directory: {directory}");
-
-                foreach (var file in folderFiles)
-                {
-                    Console.WriteLine($"File: {file}");
-                }
-
-                allFiles.AddRange(folderFiles);
-            }
-
-            Console.WriteLine($"Total amount of all files: {allFiles.Count}");
-
-            foreach (var file in allFiles)
-            {
-                using var db = new BicycleDataContext();
-                Console.WriteLine($"Reading data from file: {file}");
-
-                var bicycleDataEntries =
-                    File.ReadAllLines(file)
-                    .Skip(1)
-                    .Select(line => BicycleDataEntry.FromCsv(line))
-                    .Where(dataEntry => dataEntry != null)
-                    .ToList();
-
-                db.AddRange(bicycleDataEntries);
-                db.SaveChanges();
-                db.Dispose();
-            }
-
-            Console.WriteLine("All data read.");
-
-
-            Console.ReadLine();
         }
     }
 }
