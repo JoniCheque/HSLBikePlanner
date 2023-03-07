3f619dc91f4427681858fa66790d5041b0e46a95
JoniCheque <joni@sekki.fi>
2023-03-07 21:05:26 +0200

    chore(seeding): remove temp seeding implementation

--- HSLBackEnd/Repository/Entities/BicycleDataEntry.cs
+++ HSLBackEnd/Repository/Entities/BicycleDataEntry.cs
@@ -11,39 +11,5 @@
         public string ReturnStationName { get; set; }
         public decimal Distance { get; set; }
         public uint Duration { get; set; }
-
-        public static BicycleDataEntry FromCsv(string line)
-        {
-            string[] values = line.Split(',');
-            try
-            {
-                var bicycleDataEntry = new BicycleDataEntry();
-
-                bicycleDataEntry.Departure = Convert.ToDateTime(values[0]);
-                bicycleDataEntry.Return = Convert.ToDateTime(values[1]);
-                bicycleDataEntry.DepartureStationId = values[2];
-                bicycleDataEntry.DepartureStationName = values[3];
-                bicycleDataEntry.ReturnStationId = values[4];
-
-                bicycleDataEntry.ReturnStationName = values[5];
-
-                if (string.IsNullOrWhiteSpace(values[6]))
-                    bicycleDataEntry.Distance = 0;
-
-                else if (values[6].Contains("."))
-                    bicycleDataEntry.Distance = Convert.ToDecimal(values[6].Replace(".", ","));
-
-                else
-                    bicycleDataEntry.Distance = Convert.ToDecimal(values[6]);
-
-                bicycleDataEntry.Duration = Convert.ToUInt32(values[7]);
-
-                return bicycleDataEntry;
-            }
-            catch (Exception e)
-            {
-                return null;
-            }
-        }
     }
 }
