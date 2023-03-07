8c7bbd6598d19d05877e08255bd6ce01ad093e15
JoniCheque <joni@sekki.fi>
2023-03-07 20:53:43 +0200

    chore(seeding): add custom implementation for seeding db

---
+++ HSLBackEnd/Repository/Contexts/BicycleDataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repository.Entities;
+
+namespace Repository.Contexts
+{
+    public class BicycleDataContext : DbContext
+    {
+        public DbSet<BicycleDataEntry> BicycleDataEntries { get; set; }
+
+        public string DbPath { get; }
+
+        public BicycleDataContext()
+        {
+            var folder = Environment.SpecialFolder.Desktop;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = Path.Join(path, "bicycleDataEntries.db");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+    }
+}
---
+++ HSLBackEnd/Repository/Migrations/20230306190349_InitialCreate.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BicycleDataEntries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Departure = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Return = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DepartureStationId = table.Column<string>(type: "TEXT", nullable: false),
+                    DepartureStationName = table.Column<string>(type: "TEXT", nullable: false),
+                    ReturnStationId = table.Column<string>(type: "TEXT", nullable: false),
+                    ReturnStationName = table.Column<string>(type: "TEXT", nullable: false),
+                    Distance = table.Column<int>(type: "INTEGER", nullable: false),
+                    Duration = table.Column<uint>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BicycleDataEntries", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BicycleDataEntries");
+        }
+    }
+}
